63bea87ff3e9b9449aecef905e50e4f48d9cb48f
sam880629 <116621544+sam880629@users.noreply.github.com>
2022-10-25 13:45:35 +0800

    Add files via upload

---
+++ 第二章_練習5.1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 08:50:30 2022
+
+@author: 榮 鼎
+"""
+
+print("我設計的第一個Python程式!")
+#我設計的第一個Python程式!
+#P2-31.習題5.1
---
+++ 第二章_練習5.2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 08:54:26 2022
+
+@author: 榮 鼎
+"""
+#方法一
+print("*")
+
+print("**")
+
+print("***")
+
+print("****")
+
+print("*****\n")
+#P2-31.習題5-2
+
+
+#----------------------------------
+#方法二
+
+i = "*"
+for a in range(1,6):
+    print(a*i)

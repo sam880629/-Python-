c11915f8579fb03f718b449fa6fd7572e631b6d0
sam880629 <116621544+sam880629@users.noreply.github.com>
2022-10-25 13:47:00 +0800

    Add files via upload

---
+++ 第三章_練習7.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 08:59:32 2022
+
+@author: 榮 鼎
+"""
+
+i = float(input("請輸入圓周率的值:"))
+print("圓周率的值是:"+str(i))
+
+#P3-36.習題7
---
+++ 第三章_練習8.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 09:12:18 2022
+
+@author: 榮 鼎
+"""
+
+hight = input("請輸入身高值:")
+kg = input("請輸入體重值:")
+print("身高: "+ hight +" 公分")
+print("體重: "+ kg  + " 公斤")
+
+
+#P3-36.習題8
